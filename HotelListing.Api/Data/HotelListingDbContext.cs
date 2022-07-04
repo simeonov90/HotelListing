b5080be4c2b8885d3965987b00aaf04e43ae437a
simeonov90 <vilensimeonov@gmail.bg>
2022-07-04 19:14:58 +0300

    seed data

--- HotelListing.Api/Data/HotelListingDbContext.cs
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -12,5 +12,30 @@ namespace HotelListing.Api.Data
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahams",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id = 1,
+                    Name = "Cayman Island",
+                    ShortName = "CI"
+                }
+            );
+        }
     }
 }
