9898c30be390409afe005af55a87285a60a66088
simeonov90 <vilensimeonov@gmail.bg>
2022-07-04 15:25:34 +0300

    service -> add DbContext

---
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Api.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
