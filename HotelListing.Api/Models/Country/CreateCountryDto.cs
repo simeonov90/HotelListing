b4dd64d8f6467d6fc183f12337e063ac0df5fba1
simeonov90 <vilensimeonov@gmail.bg>
2022-07-05 17:25:26 +0300

    Dtos

---
+++ HotelListing.Api/Models/Country/CreateCountryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelListing.Api.Models.Country
+{
+    public class CreateCountryDto
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
---
+++ HotelListing.Api/Models/Hotel/HotelDto.cs
@@ -0,0 +1,14 @@
+﻿using HotelListing.Api.Models.Country;
+
+namespace HotelListing.Api.Models.Hotel
+{
+    public class HotelDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+        public int CountryId { get; set; }
+        public CountryDto Country { get; set; }
+    }
+}
